13b64c826743ebfcefcafc47ef6321939cfb89fb
ChrisEski <eschristos1@gmail.com>
2024-03-06 17:12:20 +0200

    updated authentication

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -1,6 +1,5 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
-import { WebhookEvent } from "@clerk/nextjs/server";
 import { createOrUpdateUser, deleteUser } from "@/lib/data";
 
 export async function POST(req) {
